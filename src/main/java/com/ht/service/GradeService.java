7128dede3c48365f2a3ff1ba74708c49fd29ee30
QKGOOD <1756691236@qq.com>
2017-06-01 20:05:56 +0800

    再来一遍

--- src/main/java/com/ht/service/GradeService.java
+++
@@ -1,9 +0,0 @@
-package com.ht.service;
-
-import com.ht.bean.Grade;
-
-/**
- * Created by xiao-kang on 2017/2/25.
- */
-public interface GradeService extends BaseService<Grade,Integer> {
-}
--- src/main/java/com/ht/service/ReplyService.java
+++
@@ -1,9 +0,0 @@
-package com.ht.service;
-
-import com.ht.bean.Reply;
-
-/**
- * Created by CSWangBin on 2017/3/10.
- */
-public interface ReplyService extends BaseService<Reply, Integer> {
-}
